3b61a52f11ed56ab3c798cffdf67141908a19898
YUChoe <yonguk.choe@gmail.com>
2015-09-07 14:19:58 +0900

    출력 수정. 시간 추가

--- src/wf_b.py
+++ src/wf_b.py
@@ -18,12 +18,21 @@ def csv_to_WAF_array(csvfile):
   fp = open(csvfile, 'r')
   print '%s ... processing' % (csvfile)
   c = 0
+  first_time = ''
+  until_time = ''
+
   for line in fp :
     l = line[:-1].split(',')
     if l[0] == '""' or l[0] == '"No"' : continue
     # "No","시간","장비","애플리케이션","도메인","공격명","서버","클라이언트","차단여부","증거ID","로그ID","애플리케이션ID","입력인터페이스","URL","URL인수","데이터","기타"
     key = (l[1]+'.'+l[0]).replace('"','')
-    wa_name = l[5].replace('"','')
+    if c == 0 :
+      until_time = l[1]
+    try:
+      wa_name = l[5].replace('"','')
+    except:
+      print "Parse Error: " + line
+      continue
 
     if wa_name not in WAP :
         WAP[wa_name] = {}
@@ -31,10 +40,13 @@ def csv_to_WAF_array(csvfile):
     WAP[wa_name][key] = line
     #print key, wa_name, l[8], l[16:]
 
-    c += 1
-    if c % 250 == 0 :
-      sys.stdout.write('.')
-      sys.stdout.flush()
+    #c += 1
+    #if c % 500 == 0 :
+    #  sys.stdout.write('.')
+    #  sys.stdout.flush()
+    first_time = l[1]
+
+  print "%s ~ %s" % (first_time, until_time)
 
 def write_WAF_to_file() :
   path = '../results'
@@ -45,7 +57,7 @@ def write_WAF_to_file() :
     c = 0
 
     for timeline in WAP[k] :
-      fp.write(WAP[k][timeline] + '\n')
+      fp.write(WAP[k][timeline])
       c += 1
       if c % 250 == 0 :
         sys.stdout.write('+')
